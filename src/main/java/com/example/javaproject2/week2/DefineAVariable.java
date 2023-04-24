43218d2c7ce251e06489b57fcfd95db5096f1c4a
Kyeongrok <oceanfog1@gmail.com>
2023-04-24 20:25:09 +0900

    feat: 2주차 1일차

---
+++ src/main/java/com/example/javaproject2/week2/DefineAVariable.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week2;
+
+public class DefineAVariable {
+    public static void main(String[] args) {
+        int num = 10;
+        String name = "김경록"; // primitive타입 쓰듯이 초기화
+        String name2 = new String("김미미"); // 원래는 이렇게 생김
+
+        String sOne = new String("1");
+        int iOne = 1;
+        float fOne = 1.0f;
+
+
+    }
+}
